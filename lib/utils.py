85afbb0f19102fe5c5eb59a5a8e042842b52d498
Zev Lee <60147316+zevlee@users.noreply.github.com>
2022-10-23 23:54:56 -0500

    Handle invalid configuration file error on startup

--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,7 @@
 from os.path import dirname, join, basename, normpath, expanduser
 from platform import system
 from textwrap import TextWrapper
+from json import loads, dumps
 from gi.repository import GLib
 
 
@@ -65,3 +66,53 @@ class Utils:
         """
         t = TextWrapper(width=char, break_on_hyphens=False)
         return t.fill(text)
+    
+    @staticmethod
+    def read_config(filename):
+        """
+        Given a filename `filename`, return the configuration dictionary or
+        the default configuration if `filename` is not found
+        
+        :param filename: Filename
+        :type filename: str
+        :return: Configuration dictionary
+        :rtype: dict
+        """
+        try:
+            config = loads(open(join(Utils.CONFIG_DIR, filename), "r").read())
+        except FileNotFoundError:
+            config = Utils.DEFAULT
+        return config
+    
+    @staticmethod
+    def validate_config(filename, default="RESET"):
+        """
+        Given a filename `filename`, replace the file with filename `default`
+        if it is not valid
+        
+        :param filename: Default filename
+        :type filename: str
+        :param filename: Config filename
+        :type filename: str
+        """
+        overwrite = False
+        default_config = Utils.read_config(default)
+        config = Utils.read_config(filename)
+        # Remove invalid keys
+        for key in [k for k in config.keys() if k not in Utils.DEFAULT.keys()]:
+            config.pop(key)
+            overwrite = True
+        # Add missing keys
+        for key in [k for k in Utils.DEFAULT.keys() if k not in config.keys()]:
+            config[key] = default_config[key]
+            overwrite = True
+        # Validate config options
+        for k in ["appr", "dbug"]:
+            if not isinstance(config[k], int):
+                config[k] = default_config[k]
+                overwrite = True
+        # Overwrite filename if there is an error
+        if overwrite:
+            with open(join(Utils.CONFIG_DIR, filename), "w") as c:
+                c.write(dumps(config))
+                c.close()
